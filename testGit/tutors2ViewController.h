a914587c106cc991cf2e294570d557fbbbbd2fed
Thomas Kjær Christensen <J0ness@Thomass-MacBook-Pro.local>
2014-05-07 11:43:49 +0200

    removed slider menu

--- testGit/tutors2ViewController.h
+++ testGit/tutors2ViewController.h
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
-#import "ECSlidingViewController.h"
-#import "MenuViewController.h"
 #import "Tutor.h"
 
 @interface tutors2ViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
